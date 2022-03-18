bc1c9e352a3f66e29f20ac08e41de0d87566df2b
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2022-03-18 13:34:07 +0530

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.print("Input number: ");
+        System.out.printf("%d + %d%d  + %d%d%d\n", n, n, n, n, n, n);
+    }
+    // scn.close();
+}
+
+// Accepts an integer (n) and computes the value of n+nn+nnn
---
+++ switchBulbLtCode.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class switchBulbLtCode {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        // int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            System.out.println(i * i);
+            // count++;
+            scn.close();
+        }
+        // return count;
+       
+    }
+}
