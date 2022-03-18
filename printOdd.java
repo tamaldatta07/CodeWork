b0df3f5c3cd0f51c1a562894dcd040af9c14422a
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2022-03-18 13:38:58 +0530

    Add files via upload

---
+++ printOdd.java
@@ -0,0 +1,10 @@
+public class printOdd {
+    public static void main(String[] args){
+        for(int i = 1; i<=100; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+// Print the odd numbers from 1 to 99
