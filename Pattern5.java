1e8a9d96a7c6a078466877e4e9ae57ca9eac6843
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2022-04-11 23:00:08 +0530

    Code done

---
+++ Pattern5.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Pattern5 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int row = 1, nspaces = n / 2, nstars = 1;
+
+        while (row <= n) {
+            for (int i = 1; i <= nspaces; i++) {
+                System.out.print("\t");
+            }
+            for (int i = 1; i <= nstars; i++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+
+            // preparation of next loop
+            if (row <= n / 2) {
+                nspaces = nspaces - 1;
+                nstars = nstars + 2;
+
+            } else {
+                nspaces = nspaces + 1;
+                nstars = nstars - 2;
+
+            }
+            row++;
+            scn.close();
+
+        }
+
+    }
+}
+
+// 5
+//                 *
+//         *       *       *
+// *       *       *       *       *
+//         *       *       *
+//                 *
