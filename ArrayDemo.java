36f38411615ad40eca41cd5ea5852160d3b2330d
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-01-02 13:40:54 +0530

    new topic

---
+++ ArrayDemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ArrayDemo {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        arr[0] = 55;
+        arr[1] = 90;
+        arr[2] = 37;
+        arr[3] = 28;
+        arr[4] = 87;
+
+        swap(arr, 4, 2);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
