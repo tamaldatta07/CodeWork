a6d61eb398dd525ddf0c9cdaeccf3c6c2d6d7f17
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2024-04-28 18:33:47 +0530

    code updated

--- exercise1.java
+++ exercise1.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class exercise1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         System.out.print("Input number: ");
         System.out.printf("%d + %d%d  + %d%d%d\n", n, n, n, n, n, n);
         scn.close();
     }
-     
+
 }
 
-// Accepts an integer (n) and computes the value of n+nn+nnn
+/*
+ * Accepts an integer (n) and computes the value of n+nn+nnn
+ * 10
+ * Input number: 10 + 1010 + 101010
+ */
